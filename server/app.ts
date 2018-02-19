e43294f486f609fa0b0b2d726540531cb1fc8678
Cenk Ozan Kahraman <cokahraman@hotmail.com>
2018-02-19 14:51:10 +0000

    Clients endpoint doesn't return client specific data anymore

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,6 @@
 import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
-import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
 
@@ -15,7 +14,7 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const uristring = 'mongodb://heroku_hlh9qcl2:46avndt4lkvqtdtrk6vkkq1f9c@ds239988.mlab.com:39988/heroku_hlh9qcl2';
+const uristring = 'mongodb://127.0.0.1:27017';
 mongoose.connect(uristring, function (err, response) {
   if (err) {
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
@@ -35,7 +34,5 @@ mongoose.connect(uristring, function (err, response) {
     }
   }
 });
-  // }
-// });
 
 export { app };
